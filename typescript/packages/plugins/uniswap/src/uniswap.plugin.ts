5e33975dd2ef02a9561f62980da022593843cafe
Jon Heywood <94459819+jjheywood@users.noreply.github.com>
2025-08-20 21:39:48 +0200

    feat: added bsc support to uniswap plugin (#3)

--- typescript/packages/plugins/uniswap/src/uniswap.plugin.ts
+++ typescript/packages/plugins/uniswap/src/uniswap.plugin.ts
@@ -1,16 +1,37 @@
 import { type Chain, PluginBase } from "@goat-sdk/core";
-import { arbitrum, avalanche, base, celo, mainnet, optimism, polygon, zora } from "viem/chains";
+import {
+    arbitrum,
+    avalanche,
+    base,
+    celo,
+    mainnet,
+    optimism,
+    polygon,
+    zora,
+    bsc,
+} from "viem/chains";
 import type { UniswapCtorParams } from "./types/UniswapCtorParams";
 import { UniswapService } from "./uniswap.service";
 
-const SUPPORTED_CHAINS = [mainnet, polygon, avalanche, base, optimism, zora, arbitrum, celo];
+const SUPPORTED_CHAINS = [
+    mainnet,
+    polygon,
+    avalanche,
+    base,
+    optimism,
+    zora,
+    arbitrum,
+    celo,
+    bsc,
+];
 
 export class UniswapPlugin extends PluginBase {
     constructor(params: UniswapCtorParams) {
         super("uniswap", [new UniswapService(params)]);
     }
 
-    supportsChain = (chain: Chain) => chain.type === "evm" && SUPPORTED_CHAINS.some((c) => c.id === chain.id);
+    supportsChain = (chain: Chain) =>
+        chain.type === "evm" && SUPPORTED_CHAINS.some((c) => c.id === chain.id);
 }
 
 export const uniswap = (params: UniswapCtorParams) => new UniswapPlugin(params);
